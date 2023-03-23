f0d8f114faf80b8171e12d1a1c93d39be41393f3
openclass12 <126966317+openclass12@users.noreply.github.com>
2023-03-23 16:40:03 +0100

    fin conflits

--- lib/formluraire/connexion.dart
+++ lib/formluraire/connexion.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:parent_school_followup/formluraire/inscrription.dart';
 import 'package:parent_school_followup/formluraire/resetpassword.dart';
-import 'package:parent_school_followup/services/auth_services.dart';
 //import 'package:parent_school_followup/theme.dart';
 import '../animation/animation_lancement.dart';
 import 'package:parent_school_followup/constant.dart';
@@ -16,8 +15,8 @@ import '../widget/showSnackbar.dart';
 //import 'package:parent_school_followup/services/firebase_auth.dart';
 
 late bool _passwordVissible;
-TextEditingController _emailcontroller = TextEditingController();
-TextEditingController _passcontroller = TextEditingController();
+final TextEditingController _emailcontroller = TextEditingController();
+final TextEditingController _passcontroller = TextEditingController();
 
 class ConnexionScren extends StatefulWidget {
   static String routeName = '/Connexion';
@@ -31,13 +30,6 @@ class _ConnexionScrenState extends State<ConnexionScren> {
   void initState() {
     super.initState();
     _passwordVissible = true;
-  }
-
-  @override
-  void dispose() {
-    _emailcontroller.dispose();
-    _passcontroller.dispose();
-    super.dispose();
   }
 
   @override
@@ -81,24 +73,25 @@ class _ConnexionScrenState extends State<ConnexionScren> {
             height: kDefaultPadding,
           ),
           Animation_lancement(
-            delay: 2600,
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.only(right: 8),
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, ResetPassword.routeName);
-                  },
-                  child: const Text(
-                    'mot de passe oublier ',
-                    style: TextStyle(
-                        color: kPrimaryColorIcon,
-                        fontSize: 13,
-                        fontWeight: FontWeight.w700),
-                  ),
+            delay: 3000,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "j'ai pas de compte ? ",
+                  style: TextStyle(color: kDefaultIconDarkColor, fontSize: 16),
                 ),
-              ),
+                GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                              MaterialPageRoute(builder: ((context) => InscirptionScreen()))
+                            );
+                    },
+                    child: const Text(
+                      "Inscription",
+                      style: TextStyle(color: kPrimarColor, fontSize: 16,fontWeight: FontWeight.w600),
+                    )),
+              ],
             ),
           )
         ])),
@@ -119,6 +112,15 @@ class SingForm extends StatefulWidget {
 class _SingFormState extends State<SingForm> {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final List<String> errors = [];
+  late AuthServices _authServices;
+
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    _authServices = AuthServices();
+    super.initState();
+  }
 
   void dispose() {
     super.dispose();
@@ -126,102 +128,114 @@ class _SingFormState extends State<SingForm> {
     _passcontroller.dispose();
   }
 
-  void loginUpUser() async {
-    FirebaseAuthentification(FirebaseAuth.instance).loginWidthEmailPassword(
-        email: _emailcontroller.text, password: _passcontroller.text, context: context);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Form(
-        key: fromKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            inputEmail(),
-            const SizedBox(height: 30),
-            inputPassword(),
-            const SizedBox(
-              height: 20,
-            ),
-            
-            Animation_lancement(
-              delay: 2500,
-              child: DefaultButton(
-                title: 'connexion',
-                iconData: Icons.arrow_forward_outlined,
-                onPress: loginUpUser,
-                //   if (fromKey.currentState!.validate()) {
-
-                //     // Navigator.pushReplacement(
-                //     //     context,
-                //     //     MaterialPageRoute(
-                //     //         builder: (context) => HomeNavigation()));
-                //   } else {
-                //     print("zerze");
-                //   }
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return kEmailNullError;
+                  } else if (value.isValidEmail() == false) {
+                    return kEmailInvalidError;
+                  } else if (value.isValidEmail() == false) {
+                    return null;
+                  }
+                  return null;
+                },
+                controller: _emailcontroller,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  hintText: "Enter votre mail",
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  suffixIcon: IconInput(
+                    icon: Icons.mail,
+                    onpress: () {},
+                  ),
+                  suffixIconColor: kPrimaryColorIcon,
+                ),
+              ),
+              const SizedBox(height: 30),
+              TextFormField(
+                obscureText: _passwordVissible,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return kChamVide;
+                  } else if (value.length < 8) {
+                    return kEmailInvalidError;
+                  }
+                  return null;
+                },
+                controller: _passcontroller,
+                decoration: InputDecoration(
+                  labelText: "password",
+                  hintText: "Enter votre mot de passe",
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  suffixIcon: IconInput(
+                    icon: _passwordVissible ? Icons.lock : Icons.lock_open,
+                    onpress: () {
+                      setState(() {
+                        _passwordVissible = !_passwordVissible;
+                      });
+                    },
+                  ),
+                  suffixIconColor: kPrimaryColorIcon,
+                ),
+              ),
+              Animation_lancement(
+                delay: 2600,
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, ResetPassword.routeName);
+                      },
+                      child: const Text(
+                        'mot de passe oublier ',
+                        style: TextStyle(
+                            color: kPrimaryColorIcon,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: kDefaultPadding,
+              ),
+              if (isLoading)
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircularProgressIndicator(),
               ),
             ),
-          ],
-        ));
-  }
-
-  TextFormField inputPassword() {
-    return TextFormField(
-      obscureText: _passwordVissible,
-      validator: (value) {
-        if (value!.isEmpty) {
-          return kChamVide;
-        } else if (value.length < 8) {
-          return kEmailInvalidError;
-        }
-        return null;
-      },
-      controller: _passcontroller,
-      decoration: InputDecoration(
-        labelText: "password",
-        hintText: "Enter votre mot de passe",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: IconInput(
-          icon: _passwordVissible ? Icons.lock : Icons.lock_open,
-          onpress: () {
-            setState(() {
-              _passwordVissible = !_passwordVissible;
-            });
-          },
-        ),
-        suffixIconColor: kPrimaryColorIcon,
-      ),
-    );
-  }
-
-  TextFormField inputEmail() {
-    return TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      validator: (value) {
-        if (value!.isEmpty) {
-          return kEmailNullError;
-        } else if (value.isValidEmail() == false) {
-          return kEmailInvalidError;
-        } else if (value.isValidEmail() == false) {
-          return null;
-        }
-        return null;
-      },
-      controller: _emailcontroller,
-      decoration: InputDecoration(
-        labelText: "Email",
-        hintText: "Enter votre mail",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: IconInput(
-          icon: Icons.mail,
-          onpress: () {},
-        ),
-        suffixIconColor: kPrimaryColorIcon,
-      ),
-    );
-  }
-}
+              Animation_lancement(
+                delay: 2500,
+                child: DefaultButton(
+                    title: 'connexion',
+                    iconData: Icons.arrow_forward_outlined,
+                    onPress: () async {
+                      if (formKey.currentState?.validate() ?? false) {
+                        try {
+                          setState(() {
+                            isLoading = true;
+                          });
+                          var user =
+                              await _authServices.loginWithEmailAndPassword(
+                            email: _emailcontroller.text,
+                            password: _passcontroller.text,
+                          );
 
                           setState(() {
                             isLoading = false;
